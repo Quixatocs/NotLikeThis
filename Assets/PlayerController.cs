e50e59024e915aa52b0aa3e49298c012691b20ed
Quixatocs <quixatocs@gmail.com>
2017-07-29 08:55:11 +0100

    Level Build

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -7,22 +7,31 @@ public class PlayerController : MonoBehaviour {
     public float speed = 6.0F;
     public float jumpForce = 100.0F;
     private Vector3 moveDirection = Vector3.zero;
-	
+    private bool isJumping = false;
+    private WaitForSeconds jumpWait = new WaitForSeconds(0.75f);
 
 
-  
+
+
+
     void Update()
     {
-
         moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         moveDirection = transform.TransformDirection(moveDirection);
         moveDirection *= speed;
-        if (Input.GetButtonDown("Jump"))
+        if (isJumping == false && Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce);
+            StartCoroutine(jumpTimer());
+        }
 
         transform.position += moveDirection * Time.deltaTime;
-        //moveDirection.y -= gravity * Time.deltaTime;
-       // controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private IEnumerator jumpTimer() {
+        yield return jumpWait;
+        isJumping = false;
     }
 
 
